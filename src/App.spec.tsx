d023ac3492ecef0b8aee60b9e93c1ec9d72f91ef
Lucas Gomes <lucasgomesbt@gmail.com>
2023-06-08 20:30:03 -0300

    Projeto inicial

---
+++ src/App.spec.tsx
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+describe("App Component", () => {
+  it("Should be able to show info", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Vite + React")).toBeInTheDocument();
+  });
+});
